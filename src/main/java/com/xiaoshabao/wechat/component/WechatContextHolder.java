74bdc61d0c1fdfda5856069af944907fc5a597b9
manxx <manxx5521@163.com>
2016-07-26 21:09:01 +0800

    去掉无用类

--- src/main/java/com/xiaoshabao/wechat/component/WechatContextHolder.java
+++ src/main/java/com/xiaoshabao/wechat/component/WechatContextHolder.java
@@ -4,51 +4,56 @@ import javax.servlet.http.HttpSession;
 
 import com.xiaoshabao.webframe.component.ContextHolderUtils;
 import com.xiaoshabao.wechat.bean.WechatSession;
-
 /**
  * 获取上下问工具类
  */
 public class WechatContextHolder extends ContextHolderUtils{
-	/**
-	 * 自动登录的帐号标识
-	 */
-	private final static String ACCOUNT_ID="accountId";
+	/**微信获得session标识*/
+	public final static String WECHAT_SESSION="wechat";
 	/**
 	 * 获得wecahtSession
 	 * @return
 	 */
 	public static WechatSession getWechatSession(){
 		HttpSession session = getRequest().getSession();
-		Object obj=session.getAttribute("wecahtSession");
+		Object obj=session.getAttribute(WECHAT_SESSION);
 		if(obj!=null){
 			return (WechatSession)obj; 
 		}else{
 			return null;
 		}
-		
+	}
+	/**
+	 * 设置wecahtSession
+	 * @return
+	 */
+	public static void setWechatSession(WechatSession info){
+		HttpSession session = getRequest().getSession();
+		if(info!=null){
+			session.setAttribute(WECHAT_SESSION, info);
+		}
 	}
 	/**
 	 * 获得wecahtSession
 	 * @return
 	 */
-	public static Integer getWxAccountId(){
-		HttpSession session = getRequest().getSession();
-		String accoutId=(String) session.getAttribute(ACCOUNT_ID);
-		return Integer.valueOf(accoutId) ;
-	}
-	
-	/**
-	 * 为了优雅的URL进行单独创建session
-	 * @param accountId
-	 */
-	public static void createSession(Integer accountId){
-		if(accountId!=null){
-			HttpSession session=getRequest().getSession();
-			Integer id=(Integer) session.getAttribute(ACCOUNT_ID);
-			if(id==null||id==0){
-				session.setAttribute(ACCOUNT_ID, accountId);
-			}
+	public static Integer getAccountId(){
+		WechatSession info=getWechatSession();
+		if(info==null){
+			return null;
 		}
+		return info.getAccountId();
+	}
+	/**
+	 * 获得openId
+	 * @return
+	 */
+	public static String getOpenid(){
+		WechatSession info=getWechatSession();
+		if(info==null){
+			return null;
+		}
+		return info.getOpenid();
 	}
 	
 }
