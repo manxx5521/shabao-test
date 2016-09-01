d9d083a5884356cceab62bbafa5d0ff450b0e4b5
manxx <manxx5521@163.com>
2016-09-01 14:23:12 +0800

    添加方法

--- src/main/java/com/xiaoshabao/webframe/dao/PosterDao.java
+++ src/main/java/com/xiaoshabao/webframe/dao/PosterDao.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import com.xiaoshabao.webframe.dto.PosterDto;
+import com.xiaoshabao.webframe.entity.PosterEntity;
 
 /**
  * 海报数据操作
@@ -16,4 +17,22 @@ public interface PosterDao {
 	 * @return
 	 */
 	public List<PosterDto> getPoster(@Param("type") String type,@Param("typeId")String typeId);
+	/**
+	 * 添加海报
+	 * @param poster
+	 * @return
+	 */
+	public int insertPoster(PosterEntity poster);
+	/**
+	 * 修改海报
+	 * @param poster
+	 * @return
+	 */
+	public int updatePoster(PosterEntity poster);
+	/**
+	 * 删除海报
+	 * @param poster
+	 * @return
+	 */
+	public int deletePoster(PosterEntity poster);
 }
