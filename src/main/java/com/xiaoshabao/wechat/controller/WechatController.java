b5c2e6ce832e206f55e495fd779ad2d05c62fa88
manxx <manxx5521@163.com>
2016-12-15 21:20:07 +0800

    调整格式

--- src/main/java/com/xiaoshabao/wechat/controller/WechatController.java
+++ src/main/java/com/xiaoshabao/wechat/controller/WechatController.java
@@ -1,11 +1,9 @@
 package com.xiaoshabao.wechat.controller;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Map;
 
 import javax.annotation.Resource;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -24,8 +22,6 @@ import com.xiaoshabao.wechat.util.MessageUtil;
  * 这个类用来的{@link #doGet(HttpServletRequest, HttpServletResponse) doGet}
  * 方法用来接入微信帐号, {@link #wechatPost(HttpServletResponse, HttpServletRequest)
  * wechatPost}方法用来相应微信的各种事件
- * 
- * @see com.wechat.controller.weixin.WeixinServlet WeixinServlet 这个类是柳风写的接入可参考
  */
 @Controller
 public class WechatController {
@@ -42,50 +38,48 @@ public class WechatController {
 	 * @param echostr 随机字符串
 	 * @param timestamp 时间戳
 	 * @param nonce 随机数
-	 * @throws ServletException
-	 * @throws IOException
 	 */
 	@RequestMapping(value = "/wechat/service/{uid}/dispatch", method = RequestMethod.GET, produces = "text/plain; charset=utf-8")
 	public void doGet(HttpServletResponse response,@PathVariable("uid") Integer uid,
 			String signature,String msg_signature, String echostr, String timestamp, String nonce){
 		// 初次生成的token
-		logger.info("来自微信的接入 singnature:" + signature + "---echostr:" + echostr
-				+ "----timestamp:" + timestamp + "---nonce:" + nonce);
+		logger.info("来自微信的接入 singnature:{},echostr:{},timestamp:{},nonce:{}",signature,echostr,timestamp,nonce);
 		try {
-			String result= wechatService.getIn(uid, signature,msg_signature
-					, echostr, timestamp, nonce);
+			String result= wechatService.getIn(uid, signature,msg_signature, echostr, timestamp, nonce);
 			response.getWriter().print(result);
 		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error("接入异常");
+			logger.error("接入异常",e);
 		}
 	}
 
 	/**
 	 * 响应微信事件的方法<br>
 	 * 比如关注，发送消息等
-	 * 
 	 * @param response
 	 * @param request
-	 * @throws IOException
-	 * @throws Exception
 	 */
 	@RequestMapping(value = "/wechat/service/{uid}/dispatch", method = RequestMethod.POST, produces = "application/xml; charset=utf-8")
-	public void wechatPost(HttpServletResponse response,
-			HttpServletRequest request,@PathVariable("uid") Integer uid,String nonce) throws IOException, Exception {
-		// 将请求、响应的编码均设置为UTF-8（防止中文乱码）
-		// request.setCharacterEncoding("UTF-8");
-		response.setCharacterEncoding("UTF-8");
-		logger.info("开始响应微信服务 ！");
-		// 返回值
-		String respMessage = null;
-		// xml请求解析
-		Map<String, String> requestMap = MessageUtil.parseXml(request);
-		respMessage = wechatService.coreService(uid,requestMap,nonce);
-		
-		logger.info("结束响应微信服务返回内容:" + respMessage);
-		PrintWriter out = response.getWriter();
-		out.print(respMessage);
-		out.close();
+	public void wechatPost(HttpServletResponse response,HttpServletRequest request,@PathVariable("uid") Integer uid,String nonce) {
+		PrintWriter out = null;
+		try {
+			// 将请求、响应的编码均设置为UTF-8（防止中文乱码）
+			// request.setCharacterEncoding("UTF-8");
+			response.setCharacterEncoding("UTF-8");
+			logger.info("开始响应微信服务 ！");
+			// 返回值
+			String respMessage = null;
+			// xml请求解析
+			Map<String, String> requestMap = MessageUtil.parseXml(request);
+			respMessage = wechatService.coreService(uid,requestMap,nonce);
+			
+			logger.info("结束响应微信服务返回内容:" + respMessage);
+			out = response.getWriter();
+			out.print(respMessage);
+			out.close();
+		} catch (Exception e) {
+			logger.error("响应微信请求失败,帐号：{}",uid,e);
+			out.print("fail");
+			out.close();
+		}
 	}
 }
