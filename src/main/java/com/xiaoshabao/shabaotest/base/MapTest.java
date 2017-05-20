8d0e59a863db9e798e27ad3aa8d033a0c7840746
xv <xv@xv-PC>
2017-05-21 00:32:07 +0800

    添加参数获取内容验证

--- src/main/java/com/xiaoshabao/shabaotest/base/MapTest.java
+++ src/main/java/com/xiaoshabao/shabaotest/base/MapTest.java
@@ -12,44 +12,56 @@ import org.junit.Test;
  */
 public class MapTest {
 
-  /**
-   * 遍历一个map有很多种方式,但是以以下方式性能最优。
-   * <p>如果只是遍历key值使用下边第二个方法traverseMapKey()</p>
-   * 
-   */
-  @Test
-  public void traverseMap() {
-    Map<String, String> hm = new HashMap<String, String>();
-    hm.put("111", "222");
+	/**
+	 * 获得一个不存在的变量时，返回null
+	 */
+	@Test
+	public void test() {
+		Map<String, String> params = new HashMap<String, String>();
+		String name = params.get("name");
+		System.out.println(name == null ? "是空值" : name);// 结果 是空值
+	}
 
-    Set<Map.Entry<String, String>> entrySet = hm.entrySet();
+	/**
+	 * 遍历一个map有很多种方式,但是以以下方式性能最优。
+	 * <p>
+	 * 如果只是遍历key值使用下边第二个方法traverseMapKey()
+	 * </p>
+	 * 
+	 */
+	@Test
+	public void traverseMap() {
+		Map<String, String> hm = new HashMap<String, String>();
+		hm.put("111", "222");
 
-    Iterator<Map.Entry<String, String>> iter = entrySet.iterator();
-    while (iter.hasNext()) {
-      Map.Entry<String, String> entry = iter.next();
+		Set<Map.Entry<String, String>> entrySet = hm.entrySet();
 
-      //输出内容
-      System.out.println(entry.getKey() + "\t" + entry.getValue());
-    }
-  }
+		Iterator<Map.Entry<String, String>> iter = entrySet.iterator();
+		while (iter.hasNext()) {
+			Map.Entry<String, String> entry = iter.next();
 
-  /**
-   * 只是遍历map的key值
-   */
-  @Test
-  public void traverseMapKey() {
-    Map<String, String> hm = new HashMap<String, String>();
-    hm.put("111", "222");
+			// 输出内容
+			System.out.println(entry.getKey() + "\t" + entry.getValue());
+		}
+	}
 
-    Set<String> keySet = hm.keySet();
+	/**
+	 * 只是遍历map的key值
+	 */
+	@Test
+	public void traverseMapKey() {
+		Map<String, String> hm = new HashMap<String, String>();
+		hm.put("111", "222");
 
-    Iterator<String> iter = keySet.iterator();
-    while (iter.hasNext()) {
-      String key = iter.next();
+		Set<String> keySet = hm.keySet();
 
-      //输出内容
-      System.out.println("key值为：" + key);
-    }
-  }
+		Iterator<String> iter = keySet.iterator();
+		while (iter.hasNext()) {
+			String key = iter.next();
+
+			// 输出内容
+			System.out.println("key值为：" + key);
+		}
+	}
 
 }
