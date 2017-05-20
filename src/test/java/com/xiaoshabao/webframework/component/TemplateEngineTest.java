2f5004b98b448a58410a968f3398ce66e0c3e28a
xv <xv@xv-PC>
2017-05-21 03:03:14 +0800

    freemarker示例

--- src/test/java/com/xiaoshabao/webframework/component/TemplateEngineTest.java
+++ src/test/java/com/xiaoshabao/webframework/component/TemplateEngineTest.java
@@ -2,7 +2,11 @@ package com.xiaoshabao.webframework.component;
 
 import static org.junit.Assert.*;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
@@ -16,12 +20,39 @@ public class TemplateEngineTest {
 	public void test() {
 		try {
 			Map<String,Object> params=new HashMap<String,Object>();
+			FreemarkerUser user=new FreemarkerUser();//内部类不行，必须public类
+			user.setName("老高");
+			user.setAge(40);
+			params.put("user", user);
 			
-			TemplateEngine.renderTemplate("examplet.ftl", params);
+			params.put("booleanStr", true);
+			params.put("nowDate", new Date());
+			params.put("htmlContent", "<font color='red'>我是红色字</font>");
+			
+			//列表
+			List<String> list=new ArrayList<String>();
+			list.add("java");
+			list.add("html");
+			list.add("javascript");
+			params.put("dataList", list);
+			
+			//map
+			Map<String,Object> dataMap=new HashMap<String,Object>();
+			dataMap.put("java", "你好java");
+			dataMap.put("javascript", "你好javascript");
+			params.put("dataMap", dataMap);
+			
+			String result=TemplateEngine.renderTemplate("examplet.ftl", params);
+			System.out.println(result);
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
+			fail("Not yet implemented");
 		}
-		fail("Not yet implemented");
+		
 	}
-
+	
+	
+	
 }
+
+
--- src/test/java/test/Test1.java
+++ src/test/java/test/Test1.java
@@ -1,20 +1,10 @@
 package test;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Test1 {
 	protected Logger logger=LoggerFactory.getLogger(this.getClass());
 	
-	@Test
-	public void test(){
-		Map<String,String> params=new HashMap<String, String>();
-		String name=params.get("name");
-		System.out.println(name==null?"是空值":name);
-	}
 
 }
