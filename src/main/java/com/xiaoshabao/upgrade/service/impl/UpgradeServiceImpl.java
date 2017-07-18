53a4fe9a5e7aeeaeb9914bd8193f56b382e88970
xv <xv@xv-PC>
2017-07-18 22:38:42 +0800

    1

--- src/main/java/com/xiaoshabao/upgrade/service/impl/UpgradeServiceImpl.java
+++ src/main/java/com/xiaoshabao/upgrade/service/impl/UpgradeServiceImpl.java
@@ -30,76 +30,87 @@ public abstract class UpgradeServiceImpl implements UpgradeService {
 
 	@Override
 	public AjaxResult upgradeApplication(Integer upgradeId) {
-	  UpgradeEntity upgradeEntity=getUpgradeEntity(upgradeId);
-	  return upgradeApplication(upgradeId,upgradeEntity);
+		UpgradeEntity upgradeEntity = getUpgradeEntity(upgradeId);
+		return upgradeApplication(upgradeId, upgradeEntity);
 	}
-	
-	protected AjaxResult upgradeApplication(Integer upgradeId,UpgradeEntity upgradeEntity) {
-    existsDir(upgradeEntity);// 检查文件夹是否存在
-    existsUpgradeFile(upgradeEntity);// 检查升级文件是否再temp目录
-    deleteFileTmepDir(upgradeEntity);// 删除解压的错误文件
-    unzip(upgradeEntity, UpgradeConstants.getFilePath(upgradeEntity.getServerPath()),
-        UpgradeConstants.getFileTempPath(upgradeEntity.getServerPath(),
-            upgradeEntity.getUpgradeFileName()));// 解压文件
 
-    exeSql();
-    return null;
-  }
-	
+	protected AjaxResult upgradeApplication(Integer upgradeId,
+			UpgradeEntity upgradeEntity) {
+		existsDir(upgradeEntity);// 检查文件夹是否存在
+		existsUpgradeFile(upgradeEntity);// 检查升级文件是否再temp目录
+		deleteFileTmepDir(upgradeEntity);// 删除解压的错误文件
+		unzip(upgradeEntity, UpgradeConstants.getFilePath(upgradeEntity
+				.getServerPath()), UpgradeConstants.getFileTempPath(
+				upgradeEntity.getServerPath(),
+				upgradeEntity.getUpgradeFileName()));// 解压文件
+
+		exeSql(upgradeEntity);
+		return null;
+	}
+
 	/**
 	 * 执行sql
 	 */
-	protected void exeSql(){
-	  
+	protected void exeSql(UpgradeEntity upgradeEntity) {
+		String sqlPath = UpgradeConstants.getSqlFilePath(upgradeEntity
+				.getServerPath());
+		File sqlFileRoot = new File(sqlPath);
+		if (!sqlFileRoot.exists()) {
+			
+		}
+
 	}
+
 	/**
 	 * 覆盖文件
 	 */
-	protected void overrideFiles(UpgradeEntity upgradeEntity){
-	  try {
-	    File configFile=new File(UpgradeConstants.getSpecialConfigPath(upgradeEntity.getServerPath()));
-	    if(configFile.exists()){
-	      SAXReader reader = new SAXReader();
-	      InputStream is = new FileInputStream(configFile);
-	      Document document = reader.read(is);
-	      Element root = document.getRootElement();
-	      Iterator it = root.elementIterator();
-	      while (it.hasNext()) {
-	        Element element = (Element) it.next();
-	        if(element.getName().equals("resource")){
-	          
-	        }
-	        
-	      }
-	    }
-    } catch (Exception e) {
-      throw new MsgErrorException("解析特殊文件时出现异常",e);
-    }
-	  
-    
-	}
-	
-	protected UpgradeEntity getUpgradeEntity(Integer upgradeId){
-	  UpgradeEntity upgradeEntity = this.baseDao.getDataById(
-      UpgradeEntity.class, upgradeId);
-	  if (upgradeEntity == null) {
-	    throw new MsgErrorException("未找到要升级的项目信息");
-	  }
-	  return upgradeEntity;
+	protected void overrideFiles(UpgradeEntity upgradeEntity) {
+		try {
+			File configFile = new File(
+					UpgradeConstants.getSpecialConfigPath(upgradeEntity
+							.getServerPath()));
+			if (configFile.exists()) {
+				SAXReader reader = new SAXReader();
+				InputStream is = new FileInputStream(configFile);
+				Document document = reader.read(is);
+				Element root = document.getRootElement();
+				Iterator it = root.elementIterator();
+				while (it.hasNext()) {
+					Element element = (Element) it.next();
+					if (element.getName().equals("resource")) {
+
+					}
+
+				}
+			}
+		} catch (Exception e) {
+			throw new MsgErrorException("解析特殊文件时出现异常", e);
+		}
+
 	}
 
+	protected UpgradeEntity getUpgradeEntity(Integer upgradeId) {
+		UpgradeEntity upgradeEntity = this.baseDao.getDataById(
+				UpgradeEntity.class, upgradeId);
+		if (upgradeEntity == null) {
+			throw new MsgErrorException("未找到要升级的项目信息");
+		}
+		return upgradeEntity;
+	}
 
 	/**
 	 * 判断所需目录是否存在
+	 * 
 	 * @param upgradeEntity
 	 */
 	protected abstract void existsDir(UpgradeEntity upgradeEntity);
-	
+
 	/**
-   * 判断所需升级文件是否存在
-   * @param upgradeEntity
-   */
-  protected abstract void existsUpgradeFile(UpgradeEntity upgradeEntity);
+	 * 判断所需升级文件是否存在
+	 * 
+	 * @param upgradeEntity
+	 */
+	protected abstract void existsUpgradeFile(UpgradeEntity upgradeEntity);
 
 	/**
 	 * 把zip文件解压到指定的文件夹
@@ -111,27 +122,30 @@ public abstract class UpgradeServiceImpl implements UpgradeService {
 	 */
 	protected abstract void unzip(UpgradeEntity upgradeEntity,
 			String zipFilePath, String saveFileDir);
-	
+
 	/**
 	 * 删除文件临时目录
+	 * 
 	 * @param upgradeEntity
 	 */
 	protected abstract void deleteFileTmepDir(UpgradeEntity upgradeEntity);
-	
+
 	/**
 	 * 清空文件文件
+	 * 
 	 * @param upgradeEntity
 	 */
-	protected void clearFileTmep(UpgradeEntity upgradeEntity){
-		deleteFileTmepDir(upgradeEntity);//删除解压目录
+	protected void clearFileTmep(UpgradeEntity upgradeEntity) {
+		deleteFileTmepDir(upgradeEntity);// 删除解压目录
 	}
-	
+
 	/**
 	 * 删除文件临时
+	 * 
 	 * @param upgradeEntity
 	 */
 	protected abstract void deleteFileTmep(UpgradeEntity upgradeEntity);
-	
+
 	@Override
 	public AjaxResult getUpgradeList(Integer upgradeId, String path) {
 		// TODO Auto-generated method stub
