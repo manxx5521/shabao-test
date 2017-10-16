3c241d638184a2c7594ab0e1b55a1d693eae3a03
xv <xv@xv-PC>
2017-10-16 22:28:34 +0800

    11

---
+++ src/main/java/com/xiaoshabao/shabaotest/module/http/HttpClientTest.java
@@ -0,0 +1,227 @@
+package com.xiaoshabao.shabaotest.module.http;
+
+import java.io.IOException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.collections.MapUtils;
+import org.apache.http.Consts;
+import org.apache.http.HeaderIterator;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+/**
+ * JDK发HTTP请求
+ */
+public class HttpClientTest {
+
+	/**
+	 * get请求
+	 * 
+	 * @param url
+	 * @param charset
+	 * @throws ClientProtocolException
+	 *             发送http get请求未能正常建立连接或者访问资源
+	 * @throws IOException
+	 *             发送http get请求时资源未能正常关闭！！
+	 */
+	public static String doGet(String url, String charset)
+			throws ClientProtocolException, IOException {
+		HttpGet httpGet = new HttpGet(url);
+		CloseableHttpClient httpClient = null;
+		CloseableHttpResponse response = null;
+		HttpEntity entity = null;
+		String responseContent = null;
+
+		httpGet.setHeader("Accept", "text/html, */*; q=0.01");
+		httpGet.setHeader("Accept-Encoding", "gzip, deflate,sdch");
+		httpGet.setHeader("Accept-Language", "zh-CN,zh;q=0.8");
+		httpGet.setHeader("Connection", "keep-alive");
+		httpGet.setHeader(
+				"User-Agent",
+				"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36)");
+
+		try {
+			// 创建默认的httpClient实例.
+			httpClient = HttpClients.createDefault();
+			// 执行请求
+			response = httpClient.execute(httpGet);
+			entity = response.getEntity();
+			responseContent = EntityUtils.toString(entity, charset);// 获得响应内容
+		} finally {
+			if (response != null) {
+				response.close();
+			}
+			if (httpClient != null) {
+				httpClient.close();
+			}
+		}
+		return responseContent;
+
+	}
+
+	@Test
+	public void test() throws ClientProtocolException, IOException {
+		doGet("http://weibo.com/p/1006051227328177/photos?from=page_100605&mod=TAB#place",
+				"GBK");
+	}
+
+	private static final String HTTP = "http";
+	private static final String HTTPS = "https";
+	private static SSLConnectionSocketFactory sslsf = null;
+	private static PoolingHttpClientConnectionManager cm = null;
+	private static SSLContextBuilder builder = null;
+	static {
+		try {
+			builder = new SSLContextBuilder();
+			// 全部信任 不做身份鉴定
+			builder.loadTrustMaterial(null, new TrustStrategy() {
+				@Override
+				public boolean isTrusted(X509Certificate[] x509Certificates,
+						String s) throws CertificateException {
+					return true;
+				}
+			});
+			sslsf = new SSLConnectionSocketFactory(builder.build(),
+					new String[] { "SSLv2Hello", "SSLv3", "TLSv1", "TLSv1.2" },
+					null, NoopHostnameVerifier.INSTANCE);
+			Registry<ConnectionSocketFactory> registry = RegistryBuilder
+					.<ConnectionSocketFactory> create()
+					.register(HTTP, new PlainConnectionSocketFactory())
+					.register(HTTPS, sslsf).build();
+			cm = new PoolingHttpClientConnectionManager(registry);
+			cm.setMaxTotal(200);// max connection
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testpost() {
+		Map<String, String> header = new HashMap<String, String>();
+		try {
+			post("http://weibo.com/p/1006051227328177/photos?from=page_100605&mod=TAB#place",
+					header, null, null);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * httpClient post请求
+	 * 
+	 * @param url
+	 *            请求url
+	 * @param header
+	 *            头部信息
+	 * @param param
+	 *            请求参数 form提交适用
+	 * @param entity
+	 *            请求实体 json/xml提交适用
+	 * @return 可能为空 需要处理
+	 * @throws Exception
+	 *
+	 */
+	public static String post(String url, Map<String, String> header,
+			Map<String, String> param, HttpEntity entity) throws Exception {
+		String result = "";
+		CloseableHttpClient httpClient = null;
+		try {
+			httpClient = getHttpClient();
+			HttpPost httpPost = new HttpPost(url);
+			// 设置头信息
+			if (MapUtils.isNotEmpty(header)) {
+				for (Map.Entry<String, String> entry : header.entrySet()) {
+					httpPost.addHeader(entry.getKey(), entry.getValue());
+				}
+			}
+			// 设置请求参数
+			if (MapUtils.isNotEmpty(param)) {
+				List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+				for (Map.Entry<String, String> entry : param.entrySet()) {
+					// 给参数赋值
+					formparams.add(new BasicNameValuePair(entry.getKey(), entry
+							.getValue()));
+				}
+				UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(
+						formparams, Consts.UTF_8);
+				httpPost.setEntity(urlEncodedFormEntity);
+			}
+			// 设置实体 优先级高
+			if (entity != null) {
+				httpPost.setEntity(entity);
+			}
+			HttpResponse httpResponse = httpClient.execute(httpPost);
+			int statusCode = httpResponse.getStatusLine().getStatusCode();
+			if (statusCode == HttpStatus.SC_OK) {
+				HttpEntity resEntity = httpResponse.getEntity();
+				result = EntityUtils.toString(resEntity);
+			} else {
+				readHttpResponse(httpResponse);
+			}
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			if (httpClient != null) {
+				httpClient.close();
+			}
+		}
+		return result;
+	}
+
+	public static CloseableHttpClient getHttpClient() throws Exception {
+		CloseableHttpClient httpClient = HttpClients.custom()
+				.setSSLSocketFactory(sslsf).setConnectionManager(cm)
+				.setConnectionManagerShared(true).build();
+		return httpClient;
+	}
+
+	public static String readHttpResponse(HttpResponse httpResponse)
+			throws ParseException, IOException {
+		StringBuilder builder = new StringBuilder();
+		// 获取响应消息实体
+		HttpEntity entity = httpResponse.getEntity();
+		// 响应状态
+		builder.append("status:" + httpResponse.getStatusLine());
+		builder.append("headers:");
+		HeaderIterator iterator = httpResponse.headerIterator();
+		while (iterator.hasNext()) {
+			builder.append("\t" + iterator.next());
+		}
+		// 判断响应实体是否为空
+		if (entity != null) {
+			String responseString = EntityUtils.toString(entity);
+			builder.append("response length:" + responseString.length());
+			builder.append("response content:"
+					+ responseString.replace("\r\n", ""));
+		}
+		return builder.toString();
+	}
+}
